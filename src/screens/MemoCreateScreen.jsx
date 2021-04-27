7ad5704a82e87dba2beef589b103f24aa7fefcc5
2929boy-docker <2929boy@gmail.com>
2021-04-27 22:53:31 +0900

    create edit and create

---
+++ src/screens/MemoCreateScreen.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {
+  View, ScrollView, TextInput, StyleSheet, KeyboardAvoidingView,
+} from 'react-native';
+
+import AppBar from '../components/AppBar';
+import CircleButton from '../components/CircleButton';
+
+export default function MemoCreateScreen() {
+  return (
+    <KeyboardAvoidingView style={styles.container} behavior="height">
+      <AppBar />
+      <ScrollView style={styles.inputContainer}>
+        <TextInput value="" multiline style={styles.input} />
+      </ScrollView>
+      <CircleButton name="check" />
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F0F4F8',
+  },
+  inputContainer: {
+    backgroundColor: '#ffffff',
+    flex: 1,
+    paddingVertical: 32,
+    paddingHorizontal: 27,
+  },
+  input: {
+    flex: 1,
+    fontSize: 16,
+    lineHeight: 24,
+    textAlignVertical: 'top',
+  },
+});
